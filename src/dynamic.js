cd73a19c40b578b8f6fac46fadf20fe651867f58
Abdel Hady <obad85@gmail.com>
2019-01-29 19:26:46 +0200

    Showing the dynamic css visually in the page, instead of depending on inspecting the network, for easier testing

--- src/dynamic.js
+++ src/dynamic.js
@@ -1,4 +1,16 @@
 require('../css/dynamic.css');
 console.log('Loaded dynamically');
 
-document.body.innerHTML += '<div><h1>Now dynamic chunk has been loaded</h1></div>';
+document.body.innerHTML += `
+	<div>
+		<h1>
+			Now dynamic chunk has been loaded
+			<span class='no-dynamic-yet'>
+				But css does not load
+			</span>
+			<span class='dynamic-has-loaded'>
+				& css has been loaded
+			</span>
+		</h1>
+	</div>
+`;
--- src/entry1.js
+++ src/entry1.js
@@ -3,9 +3,7 @@ console.log('Entry 1 reached');
 
 document.body.innerHTML += '<div><h1>Entry 1 content</h1></div>';
 
-setTimeout(() =>{
-	import(
-		/* webpackChunkName: "dynamic" */
-		'./dynamic'
-	);
-}, 1000);
+import(
+	/* webpackChunkName: "dynamic" */
+	'./dynamic'
+);
